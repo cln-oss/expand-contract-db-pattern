776cc77fd521e4e76525727958b68099d24626a1
cln-oss <65890025+cln-oss@users.noreply.github.com>
2020-05-29 21:26:45 +0300

    README edited

--- src/main/kubernetes/deploy.sh
+++ src/main/kubernetes/deploy.sh
@@ -2,8 +2,6 @@ kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/a
 
 kubectl proxy
 
-kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | awk '/^deployment-controller-token-/{print $1}') | awk '$1=="token:"{print $2}'
-
 kubectl apply -f "https://cloud.weave.works/k8s/scope.yaml?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 
 kubectl port-forward -n weave "$(kubectl get -n weave pod --selector=weave-scope-component=app -o jsonpath='{.items..metadata.name}')" 4040
